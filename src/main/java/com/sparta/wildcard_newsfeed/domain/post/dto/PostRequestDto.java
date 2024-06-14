4dadfded719b2655d8ffecd1c10bf4f5393ae797
Chanwon-Seo <scwonn60@gmail.com>
2024-06-15 05:23:02 +0900

    test : 게시글 등록 dto

--- src/main/java/com/sparta/wildcard_newsfeed/domain/post/dto/PostRequestDto.java
+++ src/main/java/com/sparta/wildcard_newsfeed/domain/post/dto/PostRequestDto.java
@@ -2,26 +2,26 @@ package com.sparta.wildcard_newsfeed.domain.post.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.beans.ConstructorProperties;
 import java.util.List;
 
-@Getter
-@Setter
+import static com.sparta.wildcard_newsfeed.exception.validation.ValidationGroups.NotBlankGroup;
+
+@Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class PostRequestDto {
 
     @Schema(description = "게시물 제목", example = "제목")
-    @NotBlank(message = "제목은 필수 입력 값입니다.")
+    @NotBlank(message = "제목은 필수 입력 값입니다.", groups = NotBlankGroup.class)
     private String title;
 
     @Schema(description = "게시물 내용", example = "내용")
-    @NotBlank(message = "내용은 필수 입력 값입니다.")
+    @NotBlank(message = "내용은 필수 입력 값입니다.", groups = NotBlankGroup.class)
     private String content;
 
     @Schema(description = "업로드 파일")
