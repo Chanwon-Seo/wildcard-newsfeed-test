d6d3b6a1b4add9c47040c5d27dd38252a0106410
Chanwon-Seo <scwonn60@gmail.com>
2024-06-16 21:32:06 +0900

    test : comment dto 테스트 완료

--- src/main/java/com/sparta/wildcard_newsfeed/domain/comment/dto/CommentRequestDto.java
+++ src/main/java/com/sparta/wildcard_newsfeed/domain/comment/dto/CommentRequestDto.java
@@ -2,18 +2,17 @@ package com.sparta.wildcard_newsfeed.domain.comment.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
+@Builder
+@AllArgsConstructor
 public class CommentRequestDto {
 
     @NotBlank(message = "댓글 내용은 필수 입력 값입니다.")
     @Schema(description = "댓글 내용", example = "내용")
     private String content;
-
-    public CommentRequestDto(String content) {
-        this.content = content;
-    }
 }
