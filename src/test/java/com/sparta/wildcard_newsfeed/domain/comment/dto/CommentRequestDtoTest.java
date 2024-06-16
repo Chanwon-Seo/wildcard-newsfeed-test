09baff3fa1c957ef32a550b90dd3fb3741a100c1
Chanwon-Seo <scwonn60@gmail.com>
2024-06-16 22:33:52 +0900

    test : comment dto 테스트 완료

--- src/test/java/com/sparta/wildcard_newsfeed/domain/comment/dto/CommentRequestDtoTest.java
+++ src/test/java/com/sparta/wildcard_newsfeed/domain/comment/dto/CommentRequestDtoTest.java
@@ -1,7 +1,5 @@
 package com.sparta.wildcard_newsfeed.domain.comment.dto;
 
-import com.sparta.wildcard_newsfeed.domain.user.dto.UserEmailRequestDto;
-import com.sparta.wildcard_newsfeed.exception.validation.ValidationGroups;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
@@ -10,10 +8,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Set;
-import java.util.UUID;
 
+import static com.sparta.wildcard_newsfeed.exception.validation.ValidationGroups.NotBlankGroup;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Slf4j
 @DisplayName("Comment request Dto Test")
@@ -44,7 +42,7 @@ class CommentRequestDtoTest {
                 .build();
 
         // when
-        Set<ConstraintViolation<CommentRequestDto>> violations = validator.validate(requestDto);
+        Set<ConstraintViolation<CommentRequestDto>> violations = validator.validate(requestDto, NotBlankGroup.class);
 
         // then
         assertThat(violations).isNotEmpty();
@@ -52,6 +50,4 @@ class CommentRequestDtoTest {
             assertEquals("댓글 내용은 필수 입력 값입니다.", violation.getMessage());
         }
     }
-
-
 }
